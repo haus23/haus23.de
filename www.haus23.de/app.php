2f089c66a93af98d28b51bc7364017fcc6049bd0
Micha Alt <mail@lean-stack.de>
2016-05-25 22:00:21 +0200

    Twig integrated.

--- www.haus23.de/app.php
+++ www.haus23.de/app.php
@@ -4,5 +4,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../app/views',
+));
+
+$app->get('/{name}', function ($name) use ($app) {
+    return $app['twig']->render('index.twig', array(
+        'name' => $name
+    ));
+});
+
 $app->run();
 
