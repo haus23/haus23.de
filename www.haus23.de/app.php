919afcad1818ce6b0607ff00259b2a1f8c7a25dd
Micha Alt <mail@lean-stack.de>
2016-05-29 20:12:42 +0200

    Welcome back Twig

--- www.haus23.de/app.php
+++ www.haus23.de/app.php
@@ -7,9 +7,20 @@ require_once __DIR__.'/../vendor/autoload.php';
 require '../vendor/autoload.php';
 
 $app = new \Slim\App;
-$app->get('/', function (\Slim\Http\Request $request, Response $response) {
-    $response->getBody()->write("Hello Slim World");
 
-    return $response;
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__.'/../app/views', [
+        'cache' => false
+    ]);
+
+    return $view;
+};
+
+$app->get('/', function (\Slim\Http\Request $request, Response $response) {
+    return $this->view->render($response, 'index.html.twig');
 });
 $app->run();
