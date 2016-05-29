1e087dc2f80543e1f0250d9fe8ff4f2b34578826
Micha Alt <mail@lean-stack.de>
2016-05-29 20:03:25 +0200

    Welcome Slim PHP.

--- www.haus23.de/app.php
+++ www.haus23.de/app.php
@@ -1,5 +1,15 @@
 <?php
 
+use Slim\Http\Response;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-echo '<h2>No framework here</h2>';
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function (\Slim\Http\Request $request, Response $response) {
+    $response->getBody()->write("Hello Slim World");
+
+    return $response;
+});
+$app->run();
