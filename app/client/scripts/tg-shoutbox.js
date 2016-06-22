0e857a7dfd4001026cf24b7501b31328132d8c55
Micha Alt <micha@haus23.net>
2016-06-22 11:19:13 +0200

    Minimal first shoutbox logic.

--- app/client/scripts/tg-shoutbox.js
+++ app/client/scripts/tg-shoutbox.js
@@ -1,2 +1,23 @@
 
-console.log('Shoutbox initialized.');
+var backendUrl = 'https://bot.haus23.net/shouts';
+
+// initially load and rendner the past shouts
+loadShouts();
+// get current user info
+var user = loadUser();
+
+// handler to push a new shout
+document.getElementById('btn-shout').addEventListener('click',pushShout);
+
+function loadShouts() {
+
+}
+
+function pushShout() {
+
+}
+
+function loadUser() {
+    var url = window.location + '/user';
+    console.log('location');
+}
