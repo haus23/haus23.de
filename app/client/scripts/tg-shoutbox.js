6e25f553ff2b628a03949168e65106aba12b8c21
Micha Alt <micha@haus23.net>
2016-06-22 11:42:10 +0200

    Shouting

--- app/client/scripts/tg-shoutbox.js
+++ app/client/scripts/tg-shoutbox.js
@@ -1,6 +1,9 @@
 
 var backendUrl = 'https://bot.haus23.net/shouts';
 
+// DOM caching
+var $shoutMsg = $('#shout-msg');
+
 // initially load and rendner the past shouts
 loadShouts();
 // get current user info
@@ -8,16 +11,27 @@ var url = window.location + '/user';
 var user;
 $.getJSON(url).done(function(data) {
     user = data;
-    console.log(user);
+    if( !user.anonymous ) {
+        // show shout area
+        $('.shout-area').show();
+        // handler to push a new shout
+        document.getElementById('btn-shout').addEventListener('click',pushShout);
+    }
 });
 
-// handler to push a new shout
-document.getElementById('btn-shout').addEventListener('click',pushShout);
 
 function loadShouts() {
 
 }
 
 function pushShout() {
-
+    var msg = $shoutMsg.val().trim();
+    if( user.anonymous || msg.length == 0) {
+        return;
+    }
+    $.post(backendUrl, {
+        author: user.name,
+        msg: msg
+    });
+    $shoutMsg.val('');
 }
