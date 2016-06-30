4539b7ff40d9b672f0cd90e96f45632aafb44ebc
Micha Alt <micha@haus23.net>
2016-06-30 09:23:26 +0200

    Sending and receiving new shouts.

--- www.haus23.de/assets/js/main.js
+++ www.haus23.de/assets/js/main.js
@@ -22,55 +22,38 @@ socket.on('snd shouts', function (data) {
         var $item = $($shoutItemTpl);
         $('#shout-author',$item).html(shout.author);
         $('#shout-content',$item).html(shout.msg);
-        $shoutList.prepend($item);
+        $shoutList.append($item);
     });
 });
 
-// // initially load and rendner the past shouts
-// loadShouts();
-// // get current user info
-// var url = window.location + '/user';
-// var user;
-// $.getJSON(url).done(function(data) {
-//     user = data;
-//     if( !user.anonymous ) {
-//         // show shout area
-//         $('.shout-area').show();
-//         // handler to push a new shout
-//         document.getElementById('btn-shout').addEventListener('click',pushShout);
-//     }
-// });
-//
-//
-// function loadShouts() {
-//     $.getJSON(backendUrl).done(function (data) {
-//         $.each(data, function(){
-//             var shout = this;
-//             var $item = $($shoutItemTpl);
-//             $('#shout-author',$item).html(shout.author);
-//             $('#shout-content',$item).html(shout.msg);
-//             $shoutList.prepend($item);
-//         });
-//     });
-// }
-//
-// function pushShout() {
-//     var msg = $shoutMsg.val().trim();
-//     if( user.anonymous || msg.length == 0) {
-//         return;
-//     }
-//     $.ajax({
-//         type: 'POST',
-//         url: backendUrl,
-//         contentType: "application/json; charset=utf-8",
-//         data: JSON.stringify({
-//             author: user.name,
-//             msg: msg
-//         }),
-//         dataType: 'json'
-//     });
-//     $shoutMsg.val('');
-// }
+socket.on('rcv shout', function (shout) {
+    var $item = $($shoutItemTpl);
+    $('#shout-author',$item).html(shout.author);
+    $('#shout-content',$item).html(shout.msg);
+    $shoutList.prepend($item);
+});
+
+// get current user info
+var url = window.location + '/user';
+var user;
+$.getJSON(url).done(function(data) {
+    user = data;
+    if( !user.anonymous ) {
+        // show shout area
+        $('.shout-area').show();
+        // handler to push a new shout
+        document.getElementById('btn-shout').addEventListener('click',pushShout);
+    }
+});
+
+function pushShout() {
+    var msg = $shoutMsg.val().trim();
+    if( user.anonymous || msg.length == 0) {
+        return;
+    }
+    socket.emit('snd shout', { author: user.name, msg: msg});
+    $shoutMsg.val('');
+}
 
 },{"socket.io-client":37}],2:[function(require,module,exports){
 module.exports = after
